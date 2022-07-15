24a2f889dadacb23afc9546ae5b9a32ba69bdcbc
ItonRomero <jaysonromero1218@gmail.com>
2022-07-15 12:02:53 +0800

    added dark/light functions

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ function hideNavBar() {
   const primaryNav =   document.querySelector('.primary-nav')
 //   primaryNav.classList.toggle("hide");
 
-  
   if (primaryNav .classList.contains("hide")) {
      primaryNav.classList.remove("hide")
   }
@@ -13,3 +12,24 @@ function hideNavBar() {
     primaryNav.classList.add("hide")
   }
 }
+
+
+function addTheme() {
+  document.body.classList.toggle('lightmode')
+
+
+  const element = document.getElementById("icon");
+  if (element.classList.contains("fa-toggle-off")) {
+    element.classList.remove("fa-toggle-off")
+    element.classList.add("fa-toggle-on")
+   
+  }
+  else {
+    element.classList.remove("fa-toggle-on")
+    element.classList.add("fa-toggle-off")
+    
+  }
+}
+
+
+
