13eaec300c1a3db5a438907bf67cf162d42ffd99
ItonRomero <jaysonromero1218@gmail.com>
2022-07-16 14:29:16 +0800

    added to local strorage the dark/light function

--- main.js
+++ main.js
@@ -14,22 +14,68 @@ function hideNavBar() {
 }
 
 
-function addTheme() {
-  document.body.classList.toggle('lightmode')
+// function addTheme() {
+//   document.body.classList.toggle('lightmode')
 
 
-  const element = document.getElementById("icon");
-  if (element.classList.contains("fa-toggle-off")) {
-    element.classList.remove("fa-toggle-off")
-    element.classList.add("fa-toggle-on")
+//   const element = document.getElementById("toggle-icon");
+//   if (element.classList.contains("fa-toggle-off")) {
+//     element.classList.remove("fa-toggle-off")
+//     element.classList.add("fa-toggle-on")
    
-  }
-  else {
-    element.classList.remove("fa-toggle-on")
-    element.classList.add("fa-toggle-off")
+//   }
+//   else {
+//     element.classList.remove("fa-toggle-on")
+//     element.classList.add("fa-toggle-off")
     
-  }
+//   }
+// }
+
+
+
+
+let lightMode = localStorage.getItem('lightMode'); 
+const lightModeToggle = document.querySelector('#light-mode-toggle');
+
+const enableLightMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('lightmode');
+ 
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('lightMode', 'enabled');
 }
 
 
+const disableLightMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('lightmode');
+ 
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('lightMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (lightMode === 'enabled') {
+  enableLightMode();
+}
 
+lightModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  const toggleIcon = document.querySelector("#toggle-icon");
+  lightMode = localStorage.getItem('lightMode'); 
+  
+  // if it not current enabled, enable it
+  if (lightMode !== 'enabled' && toggleIcon.classList.contains("fa-toggle-off") ) {
+    enableLightMode();
+    toggleIcon.classList.remove("fa-toggle-off")
+    toggleIcon.classList.add("fa-toggle-on")
+   
+  // if it has been enabled, turn it off  
+  } else {  
+    disableLightMode();
+    toggleIcon.classList.remove("fa-toggle-on")
+    toggleIcon.classList.add("fa-toggle-off")
+    
+  }
+});
